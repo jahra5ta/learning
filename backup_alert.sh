efe13d898dad403f43160192ea0026fc0bb49256
jahra5ta <kolokotroni.ai@gmail.com>
2024-08-30 17:45:29 +0300

    backup_alert.sh

---
+++ backup_alert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+date=$(date +"%Y%m%d")
+tar -czvf /home/yc-user/deb-alert/alert-deb_$date.tar.gz vpn-alert_1.0-1_all.deb
+cp /home/yc-user/deb-alert/alert-deb_$date.tar.gz /shared/debs
+find /shared/debs -type f -mtime +7 -delete
